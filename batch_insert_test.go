10805330c06b314486ba0458d02bf91c2f2b9413
Robert Mulley <rmulley@fastmail.fm>
2015-04-06 23:32:02 -0400

    TestInsert() function added

--- batch_insert_test.go
+++ batch_insert_test.go
@@ -82,6 +82,52 @@ func TestFlush(t *testing.T) {
 	} //if
 } //TestFlush
 
+func TestInsert(t *testing.T) {
+	var (
+		err   error
+		query string
+		dbh   *sql.DB
+	) //var
+
+	t.Parallel()
+
+	if dbh, err = sqlmock.New(); err != nil {
+		t.Fatal(err)
+	} //if
+
+	query = "INSERT INTO table_name(a, b, c) VALUES(?, ?, ?);"
+
+	bi := NewBatchInsert(dbh, 100)
+
+	for i := 0; i < 3; i++ {
+		if err = bi.Insert(
+			query,
+			[]interface{}{
+				1,
+				2,
+				3,
+			}...,
+		); err != nil {
+			t.Fatal(err)
+		} //if
+	} //for
+
+	if len(bi.bindParams) != 9 {
+		t.Log(bi.bindParams)
+		t.Fatal("bi.bindParams not properly set by bi.Insert().")
+	} //if
+
+	if bi.insertCtr != 3 {
+		t.Log(bi.insertCtr)
+		t.Fatal("bi.insertCtr not properly being set by bi.Insert().")
+	} //if
+
+	if bi.values != " VALUES(?, ?, ?),(?, ?, ?),(?, ?, ?)," {
+		t.Log(bi.values)
+		t.Fatal("bi.values not properly being set by bi.Insert().")
+	} //if
+} //TestInsert
+
 func TestSplitQuery(t *testing.T) {
 	var (
 		err   error
