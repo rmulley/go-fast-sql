7f02d40016ed0f5adc045085c25d36949faf4a0d
Robert Mulley <rmulley@fastmail.fm>
2017-11-21 21:18:53 -0600

    Open method

--- go_fast_sql_suite_test.go
+++ go_fast_sql_suite_test.go
@@ -1,22 +1,13 @@
 package fastsql_test
 
 import (
-	"os"
-	"testing"
-
 	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
+
+	"testing"
 )
 
 func TestGoFastSql(t *testing.T) {
-	var testDir = "."
-
-	if os.Getenv("CIRCLE_TEST_REPORTS") != "" {
-		testDir = os.Getenv("CIRCLE_TEST_REPORTS")
-	}
-
 	RegisterFailHandler(Fail)
-	junitReporter := reporters.NewJUnitReporter(testDir + "/junit.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "GoFastSql Suite", []Reporter{junitReporter})
+	RunSpecs(t, "GoFastSql Suite")
 }
